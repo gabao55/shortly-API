a8c46f9abc9485e2fe835d39a3afe4e3d63ab3e6
gabao55 <gabriel.s.rosin@gmail.com>
2022-10-12 16:42:21 -0300

    Creates db on postgres

---
+++ dump.sql
@@ -0,0 +1,18 @@
+-- Creates Database
+CREATE DATABASE shortly;
+
+-- Creates tables
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE urls(
+    id SERIAL PRIMARY KEY,
+    url TEXT NOT NULL,
+    "shortUrl" TEXT NOT NULL,
+    "visitCount" INTEGER NOT NULL DEFAULT 0,
+    "userId" INTEGER REFERENCES "users"("id")
+);
