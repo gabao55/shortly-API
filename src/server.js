70eeecee0071e00192c29e70a169d43292c2691d
gabao55 <gabriel.s.rosin@gmail.com>
2022-10-13 17:04:41 -0300

    Includes url routes and increases sessions' cleaning interval

--- src/server.js
+++ src/server.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRouter from './routers/auth.router.js'
+import authRouter from './routers/auth.router.js';
+import urlRouter from './routers/url.router.js';
+import { cleanSessions } from './controllers/auth.controller.js';
 dotenv.config();
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.use(authRouter)
+server.use(authRouter);
+server.use(urlRouter);
 
 server.get('/status', (req, res) => {
     res.sendStatus(200);
 });
+
+setInterval(cleanSessions, 600000);
 
 server.listen(process.env.PORT, console.log(`Magic happens on ${process.env.PORT}`))
