7bf5694ea137a2d084318369954fe6b65087397c
gabao55 <gabriel.s.rosin@gmail.com>
2022-10-14 18:30:36 -0300

    1 - implements url shortening; 2 - Implements url deletion; 3 - Implements url opening; 4 - Implements user's urls listing; 5 - Implements ranking of most visited urls

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -1,11 +1,8 @@
 import { nanoid } from "nanoid";
 import connection from "../db/db.js";
 
-
 async function shortenUrl (req, res) {
     const { url } = req.body;
-    console.log(url)
-    console.log(isStringUrl(url))
     if (!isStringUrl(url)) return res.status(422).send('Invalid url');
 
     const userId = res.locals.userId;
@@ -38,7 +35,7 @@ function isStringUrl (url) {
 }
 
 async function getShortenedUrl (req, res) {
-    const urlId = parseInt(req.params.id);
+    const urlId = req.params.id;
 
     try {    
         const urlObj = (await connection.query(
@@ -54,19 +51,103 @@ async function getShortenedUrl (req, res) {
 }
 
 async function redirectUserToUrl (req, res) {
+    const { shortUrl } = req.params;
+    try {
+        const urlData = (await connection.query(
+            'SELECT id, url, "visitCount" FROM urls WHERE "shortUrl" = $1;',
+            [shortUrl]
+        )).rows[0];
+        if (!Object.keys(urlData).length) return res.sendStatus(404);
 
+        const newVisitCount = urlData.visitCount + 1;
+
+        await connection.query(
+            'UPDATE urls SET "visitCount" = $1 WHERE id = $2;',
+            [newVisitCount, urlData.id]
+        );
+
+        return res.redirect(urlData.url);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 }
 
 async function deleteShortenedUrl (req, res) {
+    const urlId = req.params.id;
+    const { userId } = res.locals;
+    
+    try {
+        const urlExists = (await connection.query(
+            'SELECT id FROM urls WHERE id = $1;',
+            [urlId]
+        )).rows[0];
+        if (!urlExists) return res.sendStatus(404);
 
+        const urlObj = (await connection.query(
+            'SELECT id FROM urls WHERE id = $1 AND "userId" = $2;',
+            [urlId, userId]
+        )).rows[0];
+        if (!urlObj) return res.sendStatus(401);
+
+        await connection.query(
+            'DELETE FROM urls WHERE id = $1',
+            [urlObj.id]
+        )
+
+        return res.sendStatus(200);
+    } catch (error) {
+        return res.status(500).send(error)
+    }
 }
 
 async function listUsersShortenedUrls (req, res) {
+    const { userId } = res.locals;
 
+    try {
+        const user = (await connection.query(
+            'SELECT name FROM users WHERE id = $1;',
+            [userId]
+        )).rows;
+        if (!user) return res.status(404)
+
+        const urls = (await connection.query(
+            'SELECT id, "shortUrl", url, "visitCount" FROM urls WHERE "userId" = $1;',
+            [userId]
+        )).rows;
+
+        const result = {
+            id: userId,
+            name: user[0].name,
+            visitCount: 0,
+            shortenedUrls: [],
+        }
+
+        urls.forEach(url => {
+            result.visitCount += url.visitCount;
+            result.shortenedUrls.push({
+                id: url.id,
+                shortUrl: url.shortUrl,
+                url: url.url,
+                visitCount: url.visitCount,
+            })
+        });
+
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 }
 
 async function listShortenedUrlsRanking (req, res) {
+    try {
+        const ranking = (await connection.query(
+            'SELECT users.id, users.name, COUNT(urls.id) AS "linksCount", COALESCE(SUM(urls."visitCount"), 0) AS "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC, "linksCount" DESC LIMIT 10;'
+        )).rows;
 
+        return res.status(200).send(ranking)
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 }
 
 export { shortenUrl,
--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRouter from './routers/auth.router.js';
 import urlRouter from './routers/url.router.js';
-import { cleanSessions } from './controllers/auth.controller.js';
 dotenv.config();
 
 const server = express();
@@ -16,7 +15,5 @@ server.use(urlRouter);
 server.get('/status', (req, res) => {
     res.sendStatus(200);
 });
-
-setInterval(cleanSessions, 600000);
 
 server.listen(process.env.PORT, console.log(`Magic happens on ${process.env.PORT}`))
